e5dd9449e223c54636f3e7024684ed4cc07e0f97
Kentent Roth <kentencroth@yahoo.com>
2024-01-10 17:49:22 -0600

    axios setup

---
+++ src/axios/axios.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const instance = axios.create({
+	baseURL: 'http://localhost:3000',
+});
+
+instance.interceptors.request.use((request) => {
+	const token = Cookies.get('jwt');
+
+	if (token) {
+		request.headers['Authorization'] = `Bearer ${token}`;
+	}
+
+	return request;
+});
+
+export default instance;
