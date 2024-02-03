137148acaf46f088be3ad4798ba8a0e14a4c35d1
Kentent Roth <kentencroth@yahoo.com>
2024-02-03 16:41:09 -0600

    Redirect on login

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
 import instance from '../axios/axios';
 import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
 
-	const login = async () => {
+	const login = async (e: React.FormEvent) => {
+		e.preventDefault();
 		try {
 			const response = await instance.post('/users/login', {
 				email: email,
 				password: password,
 			});
 			Cookies.set('token', response.data.authToken);
-			console.log(response.data);
+			if (response.status === 200) {
+				navigate('/');
+			}
 		} catch (error) {
 			console.error(error);
 		}
@@ -23,17 +28,19 @@ const Login = () => {
 		<div className="login">
 			<div className="login_container">
 				<h1>Login</h1>
-				<input
-					type="text"
-					placeholder="Email"
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<button onClick={login}>Login</button>
+				<form className="login-form" onSubmit={login}>
+					<input
+						type="text"
+						placeholder="Email"
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button onClick={login}>Login</button>
+				</form>
 			</div>
 		</div>
 	);
